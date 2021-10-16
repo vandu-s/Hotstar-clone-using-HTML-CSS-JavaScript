a62b1596a88639e62c1231817a05872161ffa319
Vandana Singandipe <vandana.singandipe@gmail.com>
2021-10-16 15:25:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  //create Dom Element
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  //attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  //setting up images
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  //setting elements  className
+  slide.className = "slider";
+  content.className = "slider-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+setInterval(() => {
+  createSlide();
+}, 3000);
