1c90d2b48918a11869ae6c8a6c21b0aa63584606
Vandana Singandipe <vandana.singandipe@gmail.com>
2021-10-17 18:30:30 +0530

    changes names of movie names to original

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 let movies = [
   {
-    name: "falcon and the winter soldier",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Gayatri",
+    des: "Sivaji reunits with his daughter who was missing for two decades.On learning that she was kidnapped by his lookalike,Sivaji decides to bring him to book.",
     image: "images/banner/gayatribanner.jfif",
   },
   {
-    name: "loki",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Sanak",
+    des: "Trapped in a hostage crisis inside a hospital,vivaan will move mountains for his wife,Anshika.But will they survive this race against time?",
     image: "images/banner/sanakbanner.jfif",
   },
   {
-    name: "wanda vision",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Navratri-Ek Adboot Utsav",
+    des: "Celebrate in high spirits the auspicious festival of Navratri and Durga Puja with your favourite Star Parivaar characters!",
     image: "images/banner/navdurga special.jfif",
   },
   {
     name: "raya and the last dragon",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    des: "Raya,a fallen princess,must track down the legendary last dragon to stop the evil force that have returned the threaten her world.",
     image: "images/slider 4.PNG",
   },
   {
     name: "luca",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    des: "The movie is a coming of age story about the young boy experinecing an unforgetten summer filled with gelato,paste and endless scooter rides.",
     image: "images/slider 5.PNG",
   },
 ];
